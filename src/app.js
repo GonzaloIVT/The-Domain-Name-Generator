046e1c4aa437b84cca94d24191d1072ceab293cb
GonzaloIVT <gonzalo.vergara.t@gmail.com>
2021-11-01 08:42:10 +0000

    haciendolo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function Generador(){
+  let dominio="";
+  let variable_1=["El","Un"];
+  let variable_2=["Ultimo","Mejor","Peor"];
+  let extension=[".com",".cl"];
+  for (let indice_variable1=0;indice_variable1<variable_1.length;indice_variable1++){
+      for (let indice_variable2=0;indice_variable2<variable_2.length;indice_variable2++){
+          for (let indice_variable3=0;indice_variable3<extension.length;indice_variable3++){
+              dominio+=variable_1[indice_variable1]+variable_2[indice_variable2]+extension[indice_variable3];
+              console.log(dominio);
+              dominio="";
+          }
+      }
+  }
+}
+Generador();
